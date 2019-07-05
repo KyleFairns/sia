2544dd9526282c30d0cb50b88a37be4052050de5
Kyle Fairns <kyle.fairns@gmail.com>
2019-07-05 23:02:40 +0100

    Update with clearer description of BrowserStack

--- bureau/browser_setup.js
+++ bureau/browser_setup.js
@@ -19,8 +19,8 @@ function startDriver() {
  * @category Internal
  * @name driver
  * @description Uses the config to create a browser for the specifications asked for.
- * (this will only work when paired with something like BrowserStack - which involves running the scripts using virtual machines
- * that contain the browsers specified, and will run the Selenium code through it).
+ * (this will only work when paired with something like BrowserStack - remote connection to a virtual machine with the
+ * browsers specified, handled by the startDriver function, and will run the Selenium code through it natively).
  * Could also add parallel functionality within the framework itself after doing a re-haul, whereby a parallel executor
  * will wrap tests (this may end up being a mocha only feature, but with more thought may work for cucumber)
  * @type Class
